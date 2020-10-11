1137ac3b385caf07c9aa00ea283aaeff94e06f8f
kimslor <68843643+kimslor@users.noreply.github.com>
2020-10-11 15:56:01 +1300

    update

--- University Of Auckland - Assignments/COMPSCI_220_Assignment_3_2020_Graphs/CS220Assignment3Q3/main.cpp
+++ University Of Auckland - Assignments/COMPSCI_220_Assignment_3_2020_Graphs/CS220Assignment3Q3/main.cpp
@@ -3,21 +3,23 @@
 #include <sstream>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <set>
 
 using namespace std;
 
-class node {
-    public:
-        int number;
-        int num_In_Edges;
-};
+// class node {
+//     public:
+//         int number;
+//         int num_In_Edges;
+// };
 
-node* new_Node(int num) { //create new node
-    node* n = new node();
-    n->number = num; //node number
-    n->num_In_Edges = 0; //amount of in_degrees
-    return n;
-}
+// node* new_Node(int num) { //create new node
+//     node* n = new node();
+//     n->number = num; //node number
+//     n->num_In_Edges = 0; //amount of in_degrees
+//     return n;
+// }
 
 void get_First_Line(bool& end_Of_Input,int& num_N, int& k_T) { //gets number of nodes, and root index from first line and stores it in respective variables
     string first_Line; //first line of block, containing the number of nodes and the root index
@@ -52,18 +54,37 @@ string remove_Spaces(string str) { //remove spaces in a string
     return str; 
 } 
 
-bool comp(const node& n1, const node& n2) {  //comparisons for nodes based on amount of in_edges, if edges are equal then sort by smallest node number
-    if(n1.num_In_Edges == n2.num_In_Edges) {
-        return n1.number < n2.number;
+// bool comp(const node& n1, const node& n2) {  //comparisons for nodes based on amount of in_edges, if edges are equal then sort by smallest node number
+//     if(n1.num_In_Edges == n2.num_In_Edges) {
+//         return n1.number < n2.number;
+//     }
+//     else {
+//         return n1.num_In_Edges > n2.num_In_Edges;
+//     }
+// }
+
+struct comp {
+    template <typename T> 
+    bool operator()(const pair<int, int>& a, const pair<int, int>& b) {
+        if (a.second == b.second) {
+           return a.first < b.first;
+      }
+      else {
+           return a.second < b.second;
+       }
     }
-    else {
-        return n1.num_In_Edges > n2.num_In_Edges;
-    }
+};
+
+void sort(map<int, int>& M) {
+    set<pair<string, int>, comp> S(M.begin(), M.end()); 
 }
 
 int main() {
     
-    vector<int> output; //stores the values of the nodes for output
+    // vector<int> output; //stores the values of the nodes for output
+
+    vector<int> output;
+    map<int, int> n;
 
     while(1) {
         //take input first line to obtain num_nodes and k_th
@@ -75,36 +96,54 @@ int main() {
             break;
         }
 
-        node* nodes = new node[num_Nodes]; //create all given nodes
-        for(int i = 0; i < num_Nodes; i++) { //initialize the node numbers and amount of in_edges
-            nodes[i].number = i;
-            nodes[i].num_In_Edges = 0;
+        for(int i = 0; i < num_Nodes; i++) { //insert nodes into the map and intialise the in_edges to be 0
+            n.insert(pair<int, int>(i, 0));   
         }
-    
-        //read list for edges and input the number of edges to their corressponding node
+
         for(int i = 0; i < num_Nodes; i++) {
             string temp;
             getline(cin, temp);
             temp = remove_Spaces(temp);
             for(int j = 0; j < temp.length(); j++) { //for every number in the row (in_edges), increase the amount of inedges for the corressponding node
-                for(int k = 0; k < num_Nodes; k++) {
-                    if((int)temp[j]-48 == k) { //convert char into int for comparison with node number
-                        nodes[k].num_In_Edges++;
-                    }
-                }
+                int n_Node = (int)temp[j]-48;
+                n[n_Node]++;
             }
         }
 
+        // node* nodes = new node[num_Nodes]; //create all given nodes
+        // for(int i = 0; i < num_Nodes; i++) { //initialize the node numbers and amount of in_edges
+        //     nodes[i].number = i;
+        //     nodes[i].num_In_Edges = 0;
+        // }
+    
+        //read list for edges and input the number of edges to their corressponding node
+        // for(int i = 0; i < num_Nodes; i++) {
+        //     string temp;
+        //     getline(cin, temp);
+        //     temp = remove_Spaces(temp);
+        //     for(int j = 0; j < temp.length(); j++) { //for every number in the row (in_edges), increase the amount of inedges for the corressponding node
+        //         int n_Node = (int)temp[j]-48;
+        //         nodes[n_Node].num_In_Edges++;
+        //     }
+        // }
+
         //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
-        sort(nodes, nodes+num_Nodes, comp);
+        // sort(nodes, nodes+num_Nodes, comp);
+        sort(n);
+
 
         //find the k_th node from the ordered list of nodes
-        output.push_back(nodes[k_Th-1].number);
+        // output.push_back(nodes[k_Th-1].number);
+        output.push_back(n[k_Th-1]);
 
-        delete nodes; //clear nodes
+        // delete [] nodes; //clear nodes
     }
     
-    for (int i = 0; i < output.size(); i++) { //output the k_th nodes from each block
+    // for (int i = 0; i < output.size(); i++) { //output the k_th nodes from each block
+    //     cout << output[i] << endl;
+    // }
+
+    for(int i = 0; i < output.size(); i++) {
         cout << output[i] << endl;
     }
 
