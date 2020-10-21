ce75762ac3fafe5d3c063a470846c9eac04d52da
kimslor <68843643+kimslor@users.noreply.github.com>
2020-10-21 16:06:00 +1300

    update

--- University Of Auckland - Assignments/COMPSCI_220_Assignment_3_2020_Graphs/CS220Assignment3Q3/main.cpp
+++ University Of Auckland - Assignments/COMPSCI_220_Assignment_3_2020_Graphs/CS220Assignment3Q3/main.cpp
@@ -5,103 +5,78 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <queue>
 
 using namespace std;
 
-void get_First_Line(bool& end_Of_Input,int& num_N, int& k_T) { //gets number of nodes, and root index from first line and stores it in respective variables
-    string first_Line; //first line of block, containing the number of nodes and the root index
-    getline(cin, first_Line);
-
-    if(first_Line == "") { //if input is empty then no nodes end of list
-        end_Of_Input = true;
-        return;
-    }
-      
-    stringstream ss;
-    ss << first_Line; //store the string of first line as a stringstream
-
-    string temp_String;
-    int temp_Num[2];
-    int counter = 0;
-
-    while(!ss.eof()) { //keeps looking for ints until the end of the file
-        ss >> temp_String;
-        if(stringstream(temp_String) >> temp_Num[counter]) { //converts the number to an int and stores it in the arr
-            counter++;
-        }
-        temp_String = ""; //clear string for the end of stream
-    }
-
-    num_N = temp_Num[0];
-    k_T = temp_Num[1];
+vector<string> parse(string s){ //take input string and split it into sub string and then push it into a vector
+    vector<string> result; 
+    istringstream iss(s); 
+    for(string s; iss >> s; ) 
+        result.push_back(s); 
+    return result;
 }
 
-string remove_Spaces(string str) { //remove spaces in a string
-    str.erase(remove(str.begin(), str.end(), ' '), str.end()); 
-    return str; 
-} 
-
-bool cmp(pair<int, int>& n1, pair<int, int>& n2) { //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
-    if (n1.second != n2.second) {
-        return n1.second > n2.second; 
-    }
-    return n1.first < n2.first;
-} 
-  
-vector<pair<int, int> > sort_map(map<int, int>& node_Map) { //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number and then return a vector of pairs
-    // Declare vector of pairs 
-    vector<pair<int, int> > temp_V; 
-    
-    for (auto& it : node_Map) { 
-        temp_V.push_back(it); 
-    } 
-  
-    // Sort using comparator function 
-    sort(temp_V.begin(), temp_V.end(), cmp); 
-
-    return temp_V;
-} 
+vector<string> get_line() { //get the line
+    string line = "";
+    getline(cin, line);
+    return parse(line);
+}
 
 int main() {
-    
-    // vector<int> output; //stores the values of the nodes for output
-    vector<int> output;
 
-    while(1) {
-        //take input first line to obtain num_nodes and k_th
-        map<int, int> n;
-        bool at_End = false;
-        int num_Nodes, k_Th;
-        get_First_Line(at_End, num_Nodes, k_Th);
+    struct node {
+        int count = 0;
+        int index = 0;
+    };
 
-        if(at_End == true) { //reached end of input
+    struct Comp {
+        bool operator()(const node &a, const node &b) { //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
+            if (a.count != b.count) {
+                return a.count > b.count;
+            }
+            return a.index < b.index;
+        }
+    };
+
+    vector<int> output; //stores the output values for each respective block
+
+    while (1) {
+        map<string, int> counts;
+        map<int, vector<string> > entries;
+
+        vector<string> first_line = get_line();
+        if(first_line[0] == "") {
             break;
         }
-
-        for(int i = 0; i < num_Nodes; i++) { //insert nodes into the map and intialise the in_edges to be 0
-            n.insert(pair<int, int>(i, 0));   
-        }
-
-        for(int i = 0; i < num_Nodes; i++) {
-            string temp;
-            getline(cin, temp);
-            temp = remove_Spaces(temp);
-            for(int j = 0; j < temp.length(); j++) { //for every number in the row (in_edges), increase the amount of inedges for the corressponding node
-                int n_Node = (int)temp[j]-48;
-                n[n_Node]++;
+        int num_of_nodes = stoi(first_line[0]);    
+        int k = stoi(first_line[1]);
+        for (int i = 0; i < num_of_nodes; i++) {
+            vector<string> line = get_line(); //get line for the row
+            entries[i] = line;
+            for (string str:line) {
+                if (counts.find(str) == counts.end()) { //found the end of the input row
+                    counts[str] = 1;
+                } else {
+                    counts[str]++;
+                }
             }
         }
 
-        //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
-        vector<pair<int, int> > v_Temp;
-        v_Temp = sort_map(n);
-
-        //find the k_th node from the ordered list of nodes
-        output.push_back(v_Temp[k_Th-1].first);
+        map<int, vector<string>>::iterator it;
+           for (it = entries.begin(); it != entries.end(); it++ ) {
+            priority_queue<node, std::vector<node>, Comp> q{};
+            for(string str: it->second) {
+                q.push(node{counts[str], stoi(str)});
+            }
+            for (int i = 0; i < k-1; i++) {
+                q.pop();
+            }
+            output.push_back(q.top().index);
+        }
     }
-
+    
     for(int i = 0; i < output.size(); i++) {
         cout << output[i] << endl;
     }
-
 }
---
+++ University Of Auckland - Assignments/COMPSCI_220_Assignment_3_2020_Graphs/CS220Assignment3Q3/main.old.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using namespace std;
+
+vector<string> parse(string s){
+    vector<string> result; 
+    istringstream iss(s); 
+    for(string s; iss >> s; ) 
+        result.push_back(s); 
+    return result;
+}
+
+void get_First_Line(bool& end_Of_Input,int& num_N, int& k_T) { //gets number of nodes, and root index from first line and stores it in respective variables
+    string first_Line; //first line of block, containing the number of nodes and the root index
+    getline(cin, first_Line);
+
+    if(first_Line == "") { //if input is empty then no nodes end of list
+        end_Of_Input = true;
+        return;
+    }
+      
+    stringstream ss;
+    ss << first_Line; //store the string of first line as a stringstream
+
+    string temp_String;
+    int temp_Num[2];
+    int counter = 0;
+
+    while(!ss.eof()) { //keeps looking for ints until the end of the file
+        ss >> temp_String;
+        if(stringstream(temp_String) >> temp_Num[counter]) { //converts the number to an int and stores it in the arr
+            counter++;
+        }
+        temp_String = ""; //clear string for the end of stream
+    }
+
+    num_N = temp_Num[0];
+    k_T = temp_Num[1];
+}
+
+bool cmp(pair<int, int>& n1, pair<int, int>& n2) { //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
+    if (n1.second != n2.second) {
+        return n1.second > n2.second; 
+    }
+    return n1.first < n2.first;
+} 
+  
+vector<pair<int, int> > sort_map(map<int, int>& node_Map) { //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number and then return a vector of pairs
+    // Declare vector of pairs 
+    vector<pair<int, int> > temp_V; 
+    
+    for (auto& it : node_Map) { 
+        temp_V.push_back(it); 
+    } 
+  
+    // Sort using comparator function 
+    sort(temp_V.begin(), temp_V.end(), cmp); 
+
+    return temp_V;
+} 
+
+int main() {
+    
+    // vector<int> output; //stores the values of the nodes for output
+    vector<int> output;
+
+    while(1) {
+        //take input first line to obtain num_nodes and k_th
+        map<int, int> n;
+        bool at_End = false;
+        int num_Nodes, k_Th;
+        get_First_Line(at_End, num_Nodes, k_Th);
+
+        if(at_End == true) { //reached end of input
+            break;
+        }
+
+        for(int i = 0; i < num_Nodes; i++) { //insert nodes into the map and intialise the in_edges to be 0
+            n.insert(pair<int, int>(i, 0));   
+        }
+
+        for(int i = 0; i < num_Nodes; i++) {
+            string temp;
+            getline(cin, temp);
+            vector<string> temp_Split = parse(temp);
+            for(int j = 0; j < temp_Split.size(); j++) { //for every number in the row (in_edges), increase the amount of inedges for the corressponding node
+                stringstream n_N(temp_Split[j]);
+                int n_Node = 0;
+                n_N >> n_Node;
+                n[n_Node]++;
+            }
+        }
+
+        //order nodes based on amount of in_edges, if edges are equal then sort by smallest node number
+        vector<pair<int, int> > v_Temp;
+        v_Temp = sort_map(n);
+
+        //find the k_th node from the ordered list of nodes
+        output.push_back(v_Temp[k_Th-1].first);
+    }
+
+    for(int i = 0; i < output.size(); i++) {
+        cout << output[i] << endl;
+    }
+
+}
