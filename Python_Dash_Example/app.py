7c2ec7abbab5243b8c3b3baf107b676f7f9e7b2b
kimslor <68843643+kimslor@users.noreply.github.com>
2021-02-04 18:37:34 +1300

    Update app.py

--- Python_Dash_Example/app.py
+++ Python_Dash_Example/app.py
@@ -1,40 +1,82 @@
-# -*- coding: utf-8 -*-
-
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
+import time
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from dash.dependencies import Output, Input
+from plotly.subplots import make_subplots
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
+time_x = [0]
+random_y = [0]
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
+app.layout = html.Div([
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id = 'graph',
+        animate = True
+    ),
+    dcc.Interval(
+        id = 'timer',
+        interval = 500,
+        n_intervals = 0
     )
 ])
+
+# Add traces
+@app.callback(Output('graph', 'figure'),
+              Input('timer', 'n_intervals'))
+def update_figure(self):
+    time_x.append(time_x[-1] + 1)
+    random_y.append(random_y[-1] + 1)
+
+    fig = make_subplots(
+        rows = 2, cols = 2,
+        subplot_titles = ("Voltage for Temp1", "Voltage for Temp2", "Voltage for Temp3")
+    )
+
+    fig.add_trace(
+        go.Scatter(
+            x=list(time_x), 
+            y=list(random_y), 
+            mode='lines+markers', 
+            name='Temp1'
+        ),
+        row = 1, col = 1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(time_x), 
+            y=list(random_y), 
+            mode='lines+markers', 
+            name='Temp2'
+        ),
+        row = 2, col = 1,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(time_x), 
+            y=list(random_y), 
+            mode='lines+markers', 
+            name='Temp3'
+        ),
+        row = 1, col = 2,
+    )
+
+    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 1, col = 1)
+    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 2, col = 1)
+    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 1, col = 2)
+
+    fig.update_yaxes(title_text = "Temp1(mV)", range = [0, 10], row = 1, col = 1)
+    fig.update_yaxes(title_text = "Temp2(mV)", range = [0, 10], row = 2, col = 1)
+    fig.update_yaxes(title_text = "Temp3(mV)", range = [0, 10], row = 1, col = 2)
+
+    fig.update_layout(height = 700, width = 1200)
+
+    return fig
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
