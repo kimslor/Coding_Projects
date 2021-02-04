0c954670cd9595c26cb9971a8b54e0129b1da2c5
Kimsong <68843643+kimslor@users.noreply.github.com>
2021-02-04 20:00:22 +1300

    Update app.py

--- Python_Dash_Example/app.py
+++ Python_Dash_Example/app.py
@@ -1,3 +1,4 @@
+import random
 import time
 import dash
 import dash_core_components as dcc
@@ -9,9 +10,16 @@ from dash.dependencies import Output, Input
 from plotly.subplots import make_subplots
 
 app = dash.Dash(__name__)
-time_x = [0]
-random_y = [0]
 
+#list used for the axis values
+axis_x = [0]
+voltage_axis = [0]
+current_axis = [0]
+temp1_axis = [0]
+temp2_axis = [0]
+temp3_axis = [0]
+
+#page layout
 app.layout = html.Div([
     dcc.Graph(
         id = 'graph',
@@ -19,7 +27,7 @@ app.layout = html.Div([
     ),
     dcc.Interval(
         id = 'timer',
-        interval = 500,
+        interval = 1000, #1s intervals
         n_intervals = 0
     )
 ])
@@ -28,18 +36,27 @@ app.layout = html.Div([
 @app.callback(Output('graph', 'figure'),
               Input('timer', 'n_intervals'))
 def update_figure(self):
-    time_x.append(time_x[-1] + 1)
-    random_y.append(random_y[-1] + 1)
+    #increment time axis_x
+    axis_x.append(axis_x[-1] + 1)
+
+    #add new values to the lists
+    voltage_axis.append(random.randint(0,10))
+    current_axis.append(random.randint(0,10))
+    temp1_axis.append(random.randint(0,10))
+    temp2_axis.append(random.randint(0,10))
+    temp3_axis.append(random.randint(0,10))
 
     fig = make_subplots(
-        rows = 2, cols = 2,
-        subplot_titles = ("Voltage for Temp1", "Voltage for Temp2", "Voltage for Temp3")
+        rows = 2, cols = 3,
+        horizontal_spacing = 0.1,
+        #vertical_spacing = 0.25,
+        subplot_titles = ("Temp1", "Temp2", "Temp3", "Voltage", "Current",)
     )
 
     fig.add_trace(
         go.Scatter(
-            x=list(time_x), 
-            y=list(random_y), 
+            x=list(axis_x), 
+            y=list(temp1_axis), 
             mode='lines+markers', 
             name='Temp1'
         ),
@@ -47,32 +64,55 @@ def update_figure(self):
     )
     fig.add_trace(
         go.Scatter(
-            x=list(time_x), 
-            y=list(random_y), 
+            x=list(axis_x), 
+            y=list(temp2_axis), 
             mode='lines+markers', 
             name='Temp2'
+        ),
+        row = 1, col = 2,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(axis_x), 
+            y=list(temp3_axis), 
+            mode='lines+markers', 
+            name='Temp3'
+        ),
+        row = 1, col = 3,
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=list(axis_x), 
+            y=list(voltage_axis), 
+            mode='lines+markers', 
+            name='Voltage'
         ),
         row = 2, col = 1,
     )
     fig.add_trace(
         go.Scatter(
-            x=list(time_x), 
-            y=list(random_y), 
+            x=list(axis_x), 
+            y=list(current_axis), 
             mode='lines+markers', 
-            name='Temp3'
+            name='Current'
         ),
-        row = 1, col = 2,
+        row = 2, col = 2,
     )
 
-    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 1, col = 1)
-    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 2, col = 1)
-    fig.update_xaxes(title_text = "time(s)", range = [0, 10], row = 1, col = 2)
+    #updating axis ranges
+    fig.update_xaxes(title_text = "time(s)", range = [axis_x[-1]-20, axis_x[-1]+10], row = 1, col = 1)
+    fig.update_xaxes(title_text = "time(s)", range = [axis_x[-1]-20, axis_x[-1]+10], row = 2, col = 1)
+    fig.update_xaxes(title_text = "time(s)", range = [axis_x[-1]-20, axis_x[-1]+10], row = 1, col = 2)
+    fig.update_xaxes(title_text = "time(s)", range = [axis_x[-1]-20, axis_x[-1]+10], row = 2, col = 2)
+    fig.update_xaxes(title_text = "time(s)", range = [axis_x[-1]-20, axis_x[-1]+10], row = 1, col = 3)
 
-    fig.update_yaxes(title_text = "Temp1(mV)", range = [0, 10], row = 1, col = 1)
-    fig.update_yaxes(title_text = "Temp2(mV)", range = [0, 10], row = 2, col = 1)
-    fig.update_yaxes(title_text = "Temp3(mV)", range = [0, 10], row = 1, col = 2)
+    fig.update_yaxes(title_text = "Temp1(*C)", range = [0, 10], row = 1, col = 1)
+    fig.update_yaxes(title_text = "Temp2(*C)", range = [0, 10], row = 1, col = 2) 
+    fig.update_yaxes(title_text = "Temp3(*C)", range = [0, 10], row = 1, col = 3)
+    fig.update_yaxes(title_text = "Voltage(mV)", range = [0, 10], row = 2, col = 1)
+    fig.update_yaxes(title_text = "Current(mA)", range = [0, 10], row = 2, col = 2)
 
-    fig.update_layout(height = 700, width = 1200)
+    fig.update_layout(height = 500, width = 1000)
 
     return fig
 
